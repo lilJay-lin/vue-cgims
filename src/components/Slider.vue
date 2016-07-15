5ed458a657c2d2946e65c3ef4677afff7e942ee1
lilJay-lin <8293526jie@>
2016-07-16 00:02:06 +0800

    Initial commit

---
+++ src/components/Slider.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <template v-for="menu in menus">
+      <li class="" >
+
+      </li>
+    </template>
+    <li class="submenu open">
+      <a href="#">
+        <i class="icon icon-th-list"></i>
+        <span>角色管理</span> <span class="label">4</span>
+      </a>
+      <ul>
+        <li><a href="form-common.html">搜索角色</a></li>
+        <li><a href="form-validation.html">角色详情</a></li>
+        <li><a href="form-wizard.html">添加角色</a></li>
+        <li><a href="form-wizard.html">修改角色</a></li>
+      </ul>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props:{
+    menus: {
+      type: Array
+    },
+    isActive: {
+      type: String,
+      default: 'index'
+    }
+  }
+}
+</script>
