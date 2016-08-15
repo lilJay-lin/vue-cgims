7eb024a27af150b0951c8dccca1db53089f3d3eb
lilJay-lin <8293526jie@>
2016-08-16 00:12:03 +0800

    图片组件支持图片组上传

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -2,12 +2,13 @@
   <div class="uploader-identity">
       <span class="btn" :class="classObject">
         {{title}}
-        <input type="file" :name="name" :disabled="disabled" @change="onUploadFile"/>
+        <input type="file" :name="name" :disabled="disabled" :multiple="multiple" @change="onUploadFile"/>
       </span>
   </div>
 </template>
 <script type="text/ecmascript-6">
   import Server from 'src/api/server.js'
+  const forEach = require('lodash/forEach')
   export default {
     props: {
       classObject: {
@@ -17,6 +18,10 @@
             'btn-success': true
           }
         }
+      },
+      multiple: {
+        type: Boolean,
+        default: false
       },
       title: {
         type: String,
@@ -61,35 +66,46 @@
     methods: {
       onUploadFile: function (e) {
         let vm = this
-        let file = e.target.files[0]
+        let files = e.target.files
         let val = 1
         let idx = vm.files.length
-        val = vm.filter(file)
-        val ? (vm.uploadFile(file, idx), vm.readAsDataURL(file, idx), vm.files.push(file)) : vm.$dispatch('file-upload-error', vm.name, vm.filterMsg)
+        forEach(files, (file) => {
+          if (val) {
+            val = vm.filter(file)
+          }
+        })
+        val ? (vm.uploadFile(files, idx)/*, vm.readAsDataURL(file, idx)*/, vm.files.concat(files)) : vm.$dispatch('file-upload-error', vm.name, vm.filterMsg)
       },
-      readAsDataURL: function (file, idx) {
+/*
+readAsDataURL: function (file, idx) {
         let vm = this
         let fileReader = new window.FileReader()
         fileReader.onload = (e) => {
           vm.$dispatch('file-upload-review', vm.name, e.target.result, idx)
         }
         fileReader.readAsDataURL(file)
-      },
-      uploadFile: function (file, idx) {
+      },*/
+      uploadFile: function (files, idx) {
         let vm = this
         let url = vm.url
         if (url) {
           let formData = new window.FormData()
-          formData.append('theFile', file)
-          vm.$dispatch('file-upload-loading', vm.name, 'loading', idx)
+          formData.append('theFile', files)
+          forEach(files, (file, i) => {
+            vm.$dispatch('file-upload-loading', vm.name, 'loading', idx + i)
+          })
           Server.request({
             url,
             method: 'post',
             data: formData
           }).then((res) => {
-            vm.$dispatch('file-upload-success', vm.name, res.result, idx)
+            forEach(res.result.split(','), (src, i) => {
+              vm.$dispatch('file-upload-success', vm.name, src, idx + i)
+            })
           }, (res) => {
-            vm.$dispatch('file-upload-success', vm.name, '/assets/img/upload_error.png', idx)
+            forEach(files, (file, i) => {
+              vm.$dispatch('file-upload-success', vm.name, '/assets/img/upload_error.png', idx + i)
+            })
           })
         }
       }
